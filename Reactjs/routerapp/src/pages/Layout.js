fb54f981d9bf3ec7d3e33df8805b5269f7a6d409
azartechnet <azartechnet@gmail.com>
2026-04-01 18:53:51 +0530

    updated

--- Reactjs/routerapp/src/pages/Layout.js
+++ Reactjs/routerapp/src/pages/Layout.js
@@ -1,23 +1,46 @@
-import { Outlet,Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const Layout = () => {
-    return (
-        <>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/blogs">Blogs</Link>
-                </li>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          
+          {/* Brand */}
+          <Link className="navbar-brand" to="/">MyApp</Link>
 
+          {/* Toggle Button (Mobile) */}
+          <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Navbar Links */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/blogs">Blogs</Link>
+              </li>
             </ul>
-        </nav>
-        <Outlet />
-        </>
-    )
-}
+          </div>
+
+        </div>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
 export default Layout;
